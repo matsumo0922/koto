a6e8b742bb4e795325653b543d25e7be5776d674
matsumo0922 <caios.system@gmail.com>
2025-03-07 00:37:53 +0900

    update gitignore

--- composeApp/src/iosMain/kotlin/main.kt
+++
@@ -1,5 +0,0 @@
-import androidx.compose.ui.window.ComposeUIViewController
-import me.matsumo.koto.App
-import platform.UIKit.UIViewController
-
-fun MainViewController(): UIViewController = ComposeUIViewController { App() }
--- composeApp/src/jvmMain/kotlin/me/matsumo/koto/theme/Theme.jvm.kt
+++
@@ -1,7 +0,0 @@
-package me.matsumo.koto.theme
-
-import androidx.compose.runtime.Composable
-
-@Composable
-internal actual fun SystemAppearance(isDark: Boolean) {
-}
